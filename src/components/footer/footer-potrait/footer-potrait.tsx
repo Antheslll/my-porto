e1729726c5aecedde9d33149a52d9e01152340aa
Antheslll <liemanthony27@gmail.com>
2025-05-19 17:21:33 +0700

    menambahakan <span>

--- src/components/footer/footer-potrait/footer-potrait.tsx
+++ src/components/footer/footer-potrait/footer-potrait.tsx
@@ -128,7 +128,9 @@ const FooterPotrait = () => {
           </li>
           <li>
             <Link href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">
-              <LinkedInIcon />
+              <span>
+                <LinkedInIcon />
+              </span>
             </Link>
           </li>
         </ul>
